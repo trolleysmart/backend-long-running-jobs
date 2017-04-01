5d32be6e828545e069010fe300a8fc53c762ceab
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-04-01 14:08:34 +1300

    Added javascript key support

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const {
   parseServerApplicationId,
   parseServerMasterKey,
   parseServerClientKey,
+  parseServerJavascriptKey,
   parseServerFileKey,
   parseServerDatabaseUri,
   parseServerDashboardApplicationName,
@@ -17,6 +18,7 @@ const {
   parseServerApplicationId: process.env.PARSE_SERVER_APPLICATION_ID,
   parseServerMasterKey: process.env.PARSE_SERVER_MASTER_KEY,
   parseServerClientKey: process.env.PARSE_SERVER_CLIENT_KEY,
+  parseServerJavascriptKey: process.env.PARSE_SERVER_JAVASCRIPT_KEY,
   parseServerFileKey: process.env.PARSE_SERVER_FILE_KEY,
   parseServerDatabaseUri: process.env.PARSE_SERVER_DATABASE_URI,
   startParseDashboard: process.env.START_PARSE_DASHBOARD,
@@ -32,6 +34,7 @@ server.listen(serverPort, () => {
   console.log('Parse Server Application Id: ', parseServerApplicationId);
   console.log('Parse Server Master Key: ', parseServerMasterKey);
   console.log('Parse Server Client Key: ', parseServerClientKey);
+  console.log('Parse Server Javascript Key: ', parseServerJavascriptKey);
   console.log('Parse Server File Key: ', parseServerFileKey);
   console.log('Parse Server Database Uri: ', parseServerDatabaseUri);
   console.log('Parse Server Dashboard Application Name: ', parseServerDashboardApplicationName);
