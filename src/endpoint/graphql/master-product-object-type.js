94e9c120cb001ff851548f6606010ba8cb05b3dd
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-04-30 23:09:42 +1200

    Enhanced master product query

--- src/endpoint/graphql/master-product-object-type.js
+++ src/endpoint/graphql/master-product-object-type.js
@@ -6,6 +6,7 @@ import {
 } from 'graphql';
 import graphqlFields from 'graphql-fields';
 import {
+  List,
   Map,
 } from 'immutable';
 import {
@@ -70,22 +71,50 @@ function getMasterProductsObjectField() {
       },
     },
     resolve: (parent, args, context, info) => new Promise((resolve, reject) => {
+      const fields = graphqlFields(info);
       let masterProducts;
 
       MasterProductService.search(Map({
+        fields: List()
+            .concat(fields.description ? List.of('description') : List())
+            .concat(fields.barcode ? List.of('barcode') : List())
+            .concat(fields.imageUrl ? List.of('imageUrl') : List())
+            .concat(fields.tags ? List.of('tags') : List()),
         conditions: Map({
-          contains_description: args.description && args.description.trim().length > 0 ? args.description.trim() : undefined,
+          contains_description: args.description && args.description.trim()
+              .length > 0 ? args.description.trim() : undefined,
         }),
       }))
         .then((results) => {
           masterProducts = results;
 
-          return TagService.search(Map());
+          if (fields.tags && masterProducts.find(_ => !_.get('tags')
+              .isEmpty())) {
+            const tagIds = masterProducts.filterNot(_ => _.get('tags')
+                .isEmpty())
+              .flatMap(_ => _.get('tags')).toSet().toList();
+
+            return TagService.search(Map({
+              fields: List()
+                .concat(fields.tags.name ? List.of('name') : List())
+                .concat(fields.tags.weight ? List.of('weight') : List()),
+              conditions: Map({
+                ids: tagIds,
+              }),
+            }));
+          }
+
+          return List();
         })
         .then((tags) => {
-          resolve(masterProducts.map(masterProduct => masterProduct.update('tags', tagIds => tagIds.map(tagId => tags.find(tag => tag.get('id')
-              .localeCompare(tagId) === 0)))
-            .toJS()));
+          if (fields.tags) {
+            resolve(masterProducts.map(masterProduct => masterProduct.update('tags', tagIds => tagIds.map(tagId => tags.find(tag => tag.get(
+                  'id')
+                .localeCompare(tagId) === 0)))
+              .toJS()));
+          } else {
+            resolve(masterProducts.toJS());
+          }
         })
         .catch(error => reject(error));
     }),
