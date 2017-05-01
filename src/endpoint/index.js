176e11e396b4233da405b5d63d287b579c1370ee
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-05-01 21:32:53 +1200

    Added graphql schema generator

--- src/endpoint/index.js
+++ src/endpoint/index.js
@@ -1,5 +1,11 @@
 import GraphQLHTTP from 'express-graphql';
 import {
+  graphql,
+} from 'graphql';
+import {
+  introspectionQuery,
+} from 'graphql/utilities';
+import {
   getRootSchema,
 } from './graphql/root-schema';
 
@@ -8,6 +14,12 @@ function setupEndPoint(expressInstance) {
     schema: getRootSchema(),
     graphiql: true,
   }));
+
+  expressInstance.get('/graphql-schema', (request, response) => {
+    graphql(getRootSchema(), introspectionQuery)
+      .then(json => response.send(JSON.stringify(json, null, 2)))
+      .catch(error => response.status(500).send(error));
+  });
 }
 
 export {
