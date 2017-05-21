b0ece31af6cb4e7c53d5ec34b2ce2d0def2754d5
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-05-21 17:08:36 +1200

    Added AddSpecialItemToUserShoppingList mutation

--- src/endpoint/graphql/mutation/RootMutation.js
+++ src/endpoint/graphql/mutation/RootMutation.js
@@ -2,10 +2,12 @@
 
 import { GraphQLObjectType } from 'graphql';
 import addSpecialItemToUserShoppingList from './AddSpecialItemToUserShoppingList';
+import removeSpecialItemFromUserShoppingList from './RemoveSpecialItemFromUserShoppingList';
 
 export default new GraphQLObjectType({
   name: 'Mutation',
   fields: {
     addSpecialItemToUserShoppingList,
+    removeSpecialItemFromUserShoppingList,
   },
 });
