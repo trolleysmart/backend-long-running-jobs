5176c827af716c3d13f03fb2f17e9df25b9a36d7
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-06-04 14:37:37 +1200

    Enhanced shopping list query

---
+++ src/endpoint/graphql/mutation/RemoveSpecialItemsFromUserShoppingList.js
@@ -0,0 +1,55 @@
+// @flow
+
+import { List, Map } from 'immutable';
+import { GraphQLID, GraphQLString, GraphQLNonNull } from 'graphql';
+import { mutationWithClientMutationId } from 'graphql-relay';
+import { Exception } from 'micro-business-parse-server-common';
+import { ShoppingListService } from 'smart-grocery-parse-server-common';
+
+export default mutationWithClientMutationId({
+  name: 'RemoveSpecialItemsFromUserShoppingList',
+  inputFields: {
+    userId: { type: new GraphQLNonNull(GraphQLID) },
+    specialItemId: { type: new GraphQLNonNull(GraphQLID) },
+  },
+  outputFields: {
+    errorMessage: {
+      type: GraphQLString,
+    },
+  },
+  mutateAndGetPayload: async ({ userId, specialItemId }) => {
+    try {
+      const criteria = Map({
+        conditions: Map({
+          userId,
+          masterProductPriceId: specialItemId,
+          excludeItemsMarkedAsDone: true,
+          includeMasterProductPriceOnly: true,
+        }),
+      });
+
+      const result = await ShoppingListService.searchAll(criteria);
+      let shoppingListItems = List();
+
+      try {
+        result.event.subscribe(info => (shoppingListItems = shoppingListItems.push(info)));
+
+        await result.promise;
+      } finally {
+        result.event.unsubscribeAll();
+      }
+
+      if (shoppingListItems.isEmpty()) {
+        return {};
+      }
+
+      await Promise.all(
+        shoppingListItems.map(shoppingListItem => ShoppingListService.update(shoppingListItem.set('doneDate', new Date()))).toArray(),
+      );
+
+      return {};
+    } catch (ex) {
+      return { errorMessage: ex instanceof Exception ? ex.getErrorMessage() : ex };
+    }
+  },
+});
