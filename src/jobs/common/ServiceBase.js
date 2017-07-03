a84f6f6b30e681ba87eef9bb9ddc4e4de3fada1b
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-07-03 19:43:21 +1200

    Packages upgraded

--- src/jobs/common/ServiceBase.js
+++ src/jobs/common/ServiceBase.js
@@ -1,23 +1,5 @@
 // @flow
 
-import { List, Map } from 'immutable';
-import { TagService } from 'smart-grocery-parse-server-common';
 import { ServiceBase as StoreCrawlerServiceBase } from 'store-crawler';
 
-export default class ServiceBase extends StoreCrawlerServiceBase {
-  getExistingTags = async () => {
-    const result = TagService.searchAll(Map());
-
-    try {
-      let tags = List();
-
-      result.event.subscribe(info => (tags = tags.push(info)));
-
-      await result.promise;
-
-      return tags;
-    } finally {
-      result.event.unsubscribeAll();
-    }
-  };
-}
+export default class ServiceBase extends StoreCrawlerServiceBase {}
