45f591422df6907b81a0fea22e8841d5c140e26d
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-07-06 05:17:43 +1200

    Packages upgraded

--- src/jobs/countdown/CountdownService.js
+++ src/jobs/countdown/CountdownService.js
@@ -2,4 +2,11 @@
 
 import { ServiceBase } from '../common';
 
-export default class CountdownService extends ServiceBase {}
+export default class CountdownService extends ServiceBase {
+    syncTags = async (config) => {
+    const finalConfig = config || (await this.getConfig('Job'));
+    const store = await this.getStore('Countdown');
+    const storeId = store.get('id');
+
+    };
+}
