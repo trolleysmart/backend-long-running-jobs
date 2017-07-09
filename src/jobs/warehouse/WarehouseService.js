b42ab57dac70f29db06022acd4fcfb077e27e8b4
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-07-09 18:00:11 +1200

    Added job to set Warehouse store master product link to master product

--- src/jobs/warehouse/WarehouseService.js
+++ src/jobs/warehouse/WarehouseService.js
@@ -1,5 +1,30 @@
 // @flow
 
+import BluebirdPromise from 'bluebird';
+import { StoreMasterProductService } from 'smart-grocery-parse-server-common';
 import { ServiceBase } from '../common';
 
-export default class WarehouseService extends ServiceBase {}
+export default class WarehouseService extends ServiceBase {
+  setStoreMasterProductLinkToMasterProduct = async () => {
+    const store = await this.getStore('Warehouse');
+    const storeId = store.get('id');
+    const storeMasterProducts = await this.getAllStoreMasterProductsWithoutMasterProduct(storeId);
+    const splittedStoreMasterProducts = this.splitIntoChunks(storeMasterProducts, 100);
+
+    await BluebirdPromise.each(splittedStoreMasterProducts.toArray(), storeMasterProductChunks =>
+      Promise.all(storeMasterProductChunks.map(storeMasterProduct => this.setMasterProductLink(storeMasterProduct))),
+    );
+  };
+
+  setMasterProductLink = async (storeMasterProduct) => {
+    const masterProducts = await this.getMasterProducts({
+      barcode: storeMasterProduct.get('barcode'),
+    });
+
+    if (!masterProducts.isEmpty()) {
+      return;
+    }
+
+    await StoreMasterProductService.update(storeMasterProduct.set('masterProductId', masterProducts.first().get('id')));
+  };
+}
--- src/jobs/warehouse/index.js
+++ src/jobs/warehouse/index.js
@@ -1,3 +1,4 @@
 // @flow
 
 import './Warehouse';
+import './SetStoreMasterProductLinkToMasterProduct';
