38d171ba8af26a5f80f0fbf10461264aace05948
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-09-07 19:45:09 +1200

    Packages upgraded

--- src/jobs/warehouse/CrawlAndSyncProductCategoriesToStoreTags.js
+++ src/jobs/warehouse/CrawlAndSyncProductCategoriesToStoreTags.js
@@ -1,6 +1,5 @@
 // @flow
 
-import { Exception } from 'micro-business-common-javascript';
 import { WarehouseWebCrawlerService } from 'trolley-smart-store-crawler';
 
 const jobName = 'Warehouse - Crawl and sync product categories to store tags';
@@ -25,7 +24,7 @@ Parse.Cloud.job(jobName, async (request, status) => {
     log.info(`The job ${jobName} completed successfully.`);
     status.success(`The job ${jobName} completed successfully.`);
   } catch (ex) {
-    const errorMessage = ex instanceof Exception ? ex.getErrorMessage() : ex;
+    const errorMessage = ex instanceof Error ? ex.message : ex;
 
     log.error(`The job ${jobName} ended in error. Error: ${errorMessage}`);
     status.error(`The job ${jobName} ended in error. Error: ${errorMessage}`);
--- src/jobs/warehouse/CrawlProductsDetailsAndCurrentPrice.js
+++ src/jobs/warehouse/CrawlProductsDetailsAndCurrentPrice.js
@@ -1,6 +1,5 @@
 // @flow
 
-import { Exception } from 'micro-business-common-javascript';
 import { WarehouseWebCrawlerService } from 'trolley-smart-store-crawler';
 
 const jobName = 'Warehouse - Crawl products details and current price';
@@ -25,7 +24,7 @@ Parse.Cloud.job(jobName, async (request, status) => {
     log.info(`The job ${jobName} completed successfully.`);
     status.success(`The job ${jobName} completed successfully.`);
   } catch (ex) {
-    const errorMessage = ex instanceof Exception ? ex.getErrorMessage() : ex;
+    const errorMessage = ex instanceof Error ? ex.message : ex;
 
     log.error(`The job ${jobName} ended in error. Error: ${errorMessage}`);
     status.error(`The job ${jobName} ended in error. Error: ${errorMessage}`);
